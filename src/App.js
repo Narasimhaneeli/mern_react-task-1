64a7bda078af9fee0ce560f76622fbbf5563dc4f
Narasimhaneeli <narasimhaneeli143@gmail.com>
2023-10-15 00:27:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // You can style your app in App.css
+import WordCounter from './WordCounter'; // Import the WordCounter component
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Responsive Paragraph Word <br/> Counter</h1>
+      <WordCounter />
+    </div>
+  );
+}
+
+export default App;
